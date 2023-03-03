d9c1eab7f0a3447a10005dad321c9a3e7f7bb5d5
Matheus Santos <98832555+santos95mat@users.noreply.github.com>
2023-03-02 21:30:57 -0300

    feat: create decorators to take paciente and psicologo on HTML

---
+++ src/decorators/loggedPaciente.decorator.ts
@@ -0,0 +1,10 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const LoggedUser = createParamDecorator((_, ctx: ExecutionContext) => {
+  const request = ctx.switchToHttp().getRequest();
+  const paciente = request.paciente;
+
+  delete paciente.password;
+
+  return paciente;
+});
