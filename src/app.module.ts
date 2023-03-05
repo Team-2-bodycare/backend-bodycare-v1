d5f290224c4a861f359aa01a32e84c2c9d7a073d
GregorioGrGeOtOl <gregoriogrgeotavia@hotmail.com>
2023-03-05 15:38:38 -0300

    chore: add module notes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { NotesModule } from './notes/notes.module';
 import { PacientesModule } from './pacientes/pacientes.module';
 import { PsicologosModule } from './psicologos/psicologos.module';
 
 @Module({
-  imports: [PacientesModule, PsicologosModule],
+  imports: [PacientesModule, PsicologosModule, NotesModule],
   controllers: [],
   providers: [],
 })
